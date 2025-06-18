838301b0bd3c9e449741ecc949c94f2186b4c996
vigneshwhy <159418493+vigneshwhy@users.noreply.github.com>
2025-06-18 11:22:30 +0530

    Card component example

---
+++ src/components/CardSection.jsx
@@ -0,0 +1,39 @@
+import CardComponent from "./CardComponent";
+import aiImage from "../assets/stud.webp";
+
+const CardSection = () => {
+  return (
+    <div className="w-full min-h-screen flex flex-col items-center p-10 gap-6">
+      <h1 className="text-4xl font-bold text-gray-700">Courses</h1>
+      <div className="w-full h-[80%] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <CardComponent
+          image={aiImage}
+          title="AI Powered Digital Marketing"
+          description="100% Assured Placement + Stipend"
+        />
+        <CardComponent
+          image={aiImage}
+          title="AI Powered Full Stack Development"
+          description="100% Assured Placement + Stipend"
+        />
+        <CardComponent
+          image={aiImage}
+          title="AI Powered MERN Stack Development"
+          description="Placement Assistance"
+        />
+        <CardComponent
+          image={aiImage}
+          title="AI Powered Data Science"
+          description="Placement Assistance + Stipend"
+        />
+        <CardComponent
+          image={aiImage}
+          title="AI Powered Video Editing"
+          description="Placement Assistance"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CardSection;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import AppComponent from "./App";
+import CardSection from "./components/CardSection";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppComponent />
+    <CardSection />
   </StrictMode>
 );
